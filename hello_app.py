2d29b2b525908105fe520d4aae522349e1800d5a
Bell Codo <bellcodo@gmail.com>
2016-04-22 17:40:30 -0700

    Update hello_app.py

--- hello_app.py
+++ hello_app.py
@@ -4,7 +4,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    # import the magic random picker
+    import random, urllib2
+    
+    # list of things to pick from
+    url_for_list = "https://raw.githubusercontent.com/bellcodo/laughing-meme/master/typing_options.lst"
+    raw_typing_options = urllib2.urlopen(url_for_list)
+    list_of_options = raw_typing_options.read()
+    #print "DEBUG - raw typing options is %s, %s" % (raw_typing_options, list_of_options)
+    typing_options = list_of_options.split()
+    
+    # find a way to pick something
+    typing_choice = random.choice(typing_options)
+    return typing_choice
 
 if __name__ == "__main__":
     # go get the PORT from the environment
